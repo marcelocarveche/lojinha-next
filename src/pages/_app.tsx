ef863befec79bdede216b967a7a643a71ca31029
Marcelo Carveche <marcelo.carveche@estantemagica.com.br>
2021-10-27 22:03:43 -0400

    added wrapper + navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app'
 import GlobalStyle from '../styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
+import Navbar from '../components/Wrapper'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
